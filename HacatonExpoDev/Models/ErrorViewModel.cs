68ba1232d116412b0ffb45243a47a3496c64c4f5
Test <test@test.com>
2021-12-03 18:15:30 +0300

    Добавьте файлы проекта.

---
+++ HacatonExpoDev/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HacatonExpoDev.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
